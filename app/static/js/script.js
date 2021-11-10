4bf923724a6676a3addf3bd91aa4d111f0d79a16
Hung Nguyen <hung.t.nguyen04@sjsu.edu>
2021-11-10 15:43:58 -0800

    overlay

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const buttonNav = document.querySelector('#buttonNav');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const Reg = document.querySelector('.Register');
+
+buttonNav.addEventListener('click', function () {
+	if (header.classList.contains('open')) {
+		//close ham
+		header.classList.remove('open');
+		overlay.classList.add('fade-out');
+		overlay.classList.remove('fade-in');
+		Reg.classList.remove('Register__open');
+	} else {
+		header.classList.add('open');
+		overlay.classList.remove('fade-out');
+		overlay.classList.add('fade-in');
+		Reg.classList.add('Register__open');
+	}
+});
