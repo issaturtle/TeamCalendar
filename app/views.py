719b8212af9335047e95037e7b3449a7f7eeea77
Hung Nguyen <hung.t.nguyen04@sjsu.edu>
2021-12-21 13:56:10 -0800

    test

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint,render_template
 
 views = Blueprint('views', __name__) #define blueprint
 
-@views.route('/homepage')
+@views.route('/')
 def home():
     return render_template("homepage.html")
 
